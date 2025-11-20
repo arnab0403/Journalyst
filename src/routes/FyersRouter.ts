8ff119fcae6dc3a2f52b4bd25540c1ef6741ae8b
Arnab Dutta <arnabdutta8584@gmail.com>
2025-11-20 15:48:54 +0530

    Middleware added for user verify

--- src/routes/FyersRouter.ts
+++ src/routes/FyersRouter.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAuthCode,getDailyOrders,setAccessToken } from "../services/fyers.services";
+import { getAuthCode,getDailyOrders,setAccessToken, validateUserMiddleware } from "../services/fyers.services";
 
 const fyersRouter = express.Router();
 
 fyersRouter.get("/login",getAuthCode);
 fyersRouter.get("/redirecturl",setAccessToken);
-fyersRouter.get("/orders",getDailyOrders);
+fyersRouter.get("/orders",validateUserMiddleware,getDailyOrders);
+// fyersRouter.post("/place-order",validateUserMiddleware,placeOrder);
+// fyersRouter.post("/close-order",validateUserMiddleware,closeOrder);
 
 export default fyersRouter;
--- src/services/fyers.services.ts
+++ src/services/fyers.services.ts
@@ -1,11 +1,51 @@
 import { tradeNormalizers } from "../normalizer/TradeDataNormalize";
 import fyers, { FYERS_APP_APP_ID, FYERS_APP_SECRECT_KEY } from "../adapter/fyersConnect";
-import { Response,Request } from "express";
+import { Response,Request, NextFunction } from "express";
 import { getUserToken, saveUserToken } from "../utility/tokeStore";
 import dotenv from "dotenv"
 
 dotenv.config();
 
+
+export const validateUserMiddleware=(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        // geting the user id and broker name
+        const userId=req.body?.user_id;
+        const brokerName = req.body?.broker_name;
+
+        // edge cases
+        if (!brokerName || !userId) {
+            return res.status(400).json({
+                message:"Broker name or user id is missing",
+                status:"failed"
+            })
+        }
+        
+        // edge cases
+        if (brokerName!=="fyers") {
+            return res.status(400).json({
+                message:"Wrong broker name",
+                status:"failed"
+            })
+        }
+
+        const access_token = getUserToken(userId);
+        
+        // edge cases
+        if (!access_token) {
+            return res.redirect("/api/fyers/login");
+        }
+
+        req.body.access_token=access_token;
+        next();
+    } catch (error) {
+        res.status(500).json({
+            message:"Internal server error",
+            status:"failed"
+        })
+        console.log(error);
+    }
+}
 
 // getting the authcode by redirecting to fyers url 
 export function getAuthCode(req:Request,res:Response){
@@ -59,41 +99,15 @@ export const setAccessToken =async (req:Request,res:Response)=>{
 
 export const getDailyOrders=async(req:Request,res:Response)=>{
     try {
+        const access_token = req.body.access_token;
         
-        // geting the user id and broker name
-        const userId=req.body?.user_id;
-        const brokerName = req.body?.broker_name;
-
-        // edge cases
-        if (!brokerName || !userId) {
-            return res.status(400).json({
-                message:"Broker name or user id is missing",
-                status:"failed"
-            })
-        }
-        
-        // edge cases
-        if (brokerName!=="fyers") {
-            return res.status(400).json({
-                message:"Wrong broker name",
-                status:"failed"
-            })
-        }
-
-        const access_token = getUserToken(userId);
-        
-        // edge cases
-        if (!access_token) {
-            return res.redirect("/api/fyers/login");
-        }
-
         // setting the access token before any call
         await fyers.setAccessToken(access_token);   
 
         // getting the orders
         const order = await fyers.get_orders();
         const oders = order.orderBook;
-
+        console.log(order);
         // getting the normalizer & maping it
         const fyersNormalizer=tradeNormalizers["fyers"];
         const normalizeOrders = oders.map((o:any)=>fyersNormalizer(o));
@@ -112,3 +126,71 @@ export const getDailyOrders=async(req:Request,res:Response)=>{
         })
     }
 }
+
+// export const placeOrder = async(req:Request,res:Response)=>{
+//     try {
+//         const access_token = req.body.access_token;
+        
+//         // setting the access token before any call
+//         await fyers.setAccessToken(access_token);   
+
+//         const reqBody={ 
+//             "symbol": "NSE:TEXINFRA-EQ", 
+//             "qty": 1, 
+//             "type": 2, 
+//             "side": 1, 
+//             "productType": "INTRADAY", 
+//             "limitPrice": 0, 
+//             "stopPrice": 0, 
+//             "validity": "DAY", 
+//             "stopLoss": 0, 
+//             "takeProfit": 0, 
+//             "offlineOrder": false, 
+//             "disclosedQty": 0 
+//         }
+
+//         const response = await fyers.place_order(reqBody);
+//         console.log(response);
+//         res.status(200).json({
+//             message:"order successful",
+//             status:"success",
+//             data:response
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({
+//             message:"Internal server error",
+//             status:"failed",
+//         })
+//     }
+// }
+
+
+// export const closeOrder = async(req:Request,res:Response)=>{
+//     try {
+//         const access_token = req.body.access_token;
+        
+//         // setting the access token before any call
+//         await fyers.setAccessToken(access_token);   
+
+//         const reqBody={
+//             "id":"NSE:SBIN-EQ-INTRADAY"
+//         }
+
+//         const response = await fyers.exit_position(reqBody);
+//         console.log(response);
+
+//         res.status(200).json({
+//             message:"order successful",
+//             status:"success",
+//             data:response
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({
+//             message:"Internal server error",
+//             status:"failed",
+//         })
+//     }
+// }
+
